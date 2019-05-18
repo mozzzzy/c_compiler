3187a544c1fef605904dfa7374750f81867cbc96
mozzzzy <kurt.goedel.1109@gmail.com>
2019-05-18 07:25:41 +0000

    feat: compile the program that has + and -

--- src/9cc.c
+++ src/9cc.c
@@ -11,6 +11,9 @@ int main (int argc, char **argv) {
     return 1;
   }
 
+  // get source code from argv[1]
+  char *p = argv[1];
+
   // command to specify the syntax of assembly
   printf(".intel_syntax noprefix\n");
   // this means main function is in global scope, not file scope
@@ -18,10 +21,32 @@ int main (int argc, char **argv) {
 
   // main function
   printf("main:\n");
-  // move integer that is specified in argv[1] to rax.
+  // move the first integer that is specified in argv[1] to rax.
   // RAX is a register. the data in RAX when the function returns
-  // is used to return value of the function
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  // is used to return value of the function.
+  //
+  // NOTE: strtol update the pointer that is specified in second parameter
+  //       to the next character of last read
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      ++p;
+      // adds the integer to the right of '+' to rax.
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+    if (*p == '-') {
+      ++p;
+      // substracts the integer to the right of '+' from rax.
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "unexpected character: '%c'\n", *p);
+    return 1;
+  }
+
   // return
   printf("  ret\n");
   return 0;
